07927f495467b289ee417100a8591c023a5c8f8a
Kos <ko>
2016-11-21 08:59:23 +0400

    исправлена проблема разметки на api16

--- app/src/main/java/com/tytosoft/delivery/views/FABwithText.java
+++ app/src/main/java/com/tytosoft/delivery/views/FABwithText.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.design.widget.FloatingActionButton;
 import android.util.AttributeSet;
 
@@ -31,6 +32,9 @@ public class FABwithText extends FloatingActionButton {
 
 	Paint pen=new Paint();
 	Paint penBack=new Paint();
+	RectF textRect=new RectF();
+
+
 	private void init(Context context) {
 		float sp=context.getResources().getDisplayMetrics().scaledDensity;
 		pen.setColor(Color.BLACK);
@@ -48,16 +52,33 @@ public class FABwithText extends FloatingActionButton {
 		int h=canvas.getHeight();
 		if (text!=null) {
 
-			float tw=pen.measureText(text);
+
 			float tr=pen.getTextSize();
-			canvas.drawRoundRect((w-tw)/2-tr/4,h/2-tr,(w+tw)/2+tr/4,h/2+tr/4,tr,tr,penBack);
+			canvas.drawRoundRect(textRect,tr,tr,penBack);
 			canvas.drawText(text, w / 2, h / 2, pen);
 		}
 
 	}
 
+
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+
+		resizeRect(w, h);
+	}
+
+	private void resizeRect(int w, int h) {
+		float tw=text!=null? pen.measureText(text):1;
+		float tr=pen.getTextSize();
+		textRect=new RectF((w-tw)/2-tr/4,h/2-tr,(w+tw)/2+tr/4,h/2+tr/4);
+	}
+
 	public void setText(String text){
 		this.text=text;
+
+		resizeRect(getWidth(),getHeight());
 		invalidate();
 	}
 }
